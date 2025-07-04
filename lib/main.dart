e2ac50d565e4bc962d7594a4bc6f3cf789a874d2
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-07-04 13:49:11 +0530

    recipe app v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_recipe_app/screens/recipe_screen.dart';
 
 void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const RecipeScreen(),
+    );
+  }
 }
